d3ab0dac029e0bd40221a1ffc515f33b3da4fe98
Kristofers Petersons <kristofersp2@gmail.com>
2021-11-24 18:51:49 +0200

    Finished the first exercise.

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == 15 || num2 == 15) 
+            {
+                Console.WriteLine("True");
+            } else if (num1 + num2 == 15 || num1 - num2 == 15 || num2 - num1 == 15)
+            {
+                Console.WriteLine("True");
+            } else 
+            {
+                Console.WriteLine("False");
+            }
+            
+        }
+    }
+}
