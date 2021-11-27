49a7c3e1d6bebf5903e05e46178d8191f6047308
Kristofers Petersons <kristofersp2@gmail.com>
2021-11-27 14:26:37 +0200

    Exercise4 completed.

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = "Kristofers Petersons";
+int yearBornIn = 2001;
+
+Console.WriteLine($"My name is {name}  and I was born in {yearBornIn}.");
+
+//Tried the new .NET 6.0 framework for this exercise.
