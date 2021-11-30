5800dc465591743e12e816407f618fe35e98943f
Kristofers Petersons <kristofersp2@gmail.com>
2021-11-30 16:43:19 +0200

    Exercise7 done.

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -11,7 +11,8 @@ namespace GravityCalculator
             double initialVelocity = 0.0;
             double fallingTime = 10.0;
             double initialPosition = 0.0;
-            double finalPosition = 0.0;
+            double finalPosition = (gravity * fallingTime * fallingTime)/2 ;
+
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
         }
