8ff643ccc8a7bcb3efb1ca54f500c1268c118e36
Kristofers Petersons <kristofersp2@gmail.com>
2021-11-30 16:03:59 +0200

    Exercise4 completed.

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Programm
+    {
+        static void Main()
+        {
+            int sum = 1;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
