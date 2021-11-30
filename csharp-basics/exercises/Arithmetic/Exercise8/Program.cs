8e7cae04cc01d3c8e4c6402105e7ae2959227296
Kristofers Petersons <kristofersp2@gmail.com>
2021-11-30 17:08:37 +0200

    Exercise8 done.

---
+++ csharp-basics/exercises/Arithmetic/Exercise8/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            payment((decimal)7.50, 35);
+            payment((decimal)8.20, 47);
+            payment((decimal)10.00, 73);
+        }
+
+        public static void payment(decimal basePay, int hoursWorked)
+        {
+            int bonusHours;
+            decimal salary;
+
+            if (basePay < 8 || hoursWorked > 60)
+            {
+                Console.WriteLine("ERROR");
+            }
+            else if (hoursWorked > 40)
+            {
+                bonusHours = hoursWorked - 40;
+                salary = 40 * basePay + bonusHours * basePay * (decimal)1.5;
+
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
