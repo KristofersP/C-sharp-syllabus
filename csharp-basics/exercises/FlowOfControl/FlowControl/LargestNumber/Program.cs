d2808bf46b408279d8a27fc0b0b3d38ab040769c
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-04 13:39:59 +0200

    Exercise1 done.

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -4,24 +4,31 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            var input1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var input2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            var input3 = Convert.ToInt32(Console.ReadLine());
+            
+            if (input1 > input2 && input1 > input3)
+            {
+                Console.WriteLine(input1 + " Is the biggest number");
+            }
+            else if(input2 > input3 && input2 > input1)
+            {
+                Console.WriteLine(input2 + " Is the biggest number");
+            }
+            else
+            {
+                Console.WriteLine(input3 + " Is the biggest number");
+            }
+            Console.ReadKey();
+            
         }
     }
 }
