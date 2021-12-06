c5fb56cd424c0eb421b5be4bd2c52a9e7da2ab01
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-06 15:43:13 +0200

    Erased unnecessary comments.

--- csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
@@ -3,9 +3,7 @@
 namespace Exercise2
 {
     class Program
-    {       
-        // TODO: Write a C# program to sum values of an array.
-        // Array values are starting from min (including) till max (including) number. 
+    {
         private static void Main(string[] args)
         {
             var sum = 0;
