090ab7d8db63e7c74306f809351a97a48dc88a41
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-06 17:08:12 +0200

    Exercise6 completed.

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        //TODO: Write a C# program to find the index of an array element.
+        private static void Main(string[] args)
+        {
+            int[] myArray1 = new int[10];
+            int[] myArray2 = new int[10];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                myArray1[i] = rnd.Next(1, 101);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                myArray2[i] = myArray1[1];
+            }
+
+            myArray1[9] = -7;
+
+            Console.WriteLine("Array 1: " + string.Join(",", myArray1));
+            Console.WriteLine("Array 2: " + string.Join(",", myArray2));
+        }
+    }
+}
