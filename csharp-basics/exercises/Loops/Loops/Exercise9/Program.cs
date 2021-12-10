53ac5345c4b619065c32806b2a68fbbbfb03365d
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-10 21:21:05 +0200

    Exercise9 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RollTwoDice
+{
+    class Program
+    {
+        //TODO: print all vowels using for and foreach
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your desired sum(1 - 12):");
+            int desiredSum = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            int dice1 = 0;
+            int dice2 = 0;
+
+            while(dice1 + dice2 != desiredSum)
+            {
+                dice1 = rnd.Next(1, 7);
+                dice2 = rnd.Next(1, 7);
+
+                Console.WriteLine($"{dice1} and {dice2} = {dice1 + dice2}");
+            }
+        }
+    }
+}
