0b13a5c849dfcb6928518ea65d61fc56392bf68f
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-14 16:13:44 +0200

    Exercise 3 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3_/Odometer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_
+{
+    class Odometer
+    {
+        private int _currentMileage;
+        private int _mileageCounter = 0;
+
+        public Odometer(int startMileage)
+        {
+            _currentMileage = startMileage;
+        }
+
+        public void ShowMileage()
+        {
+            Console.WriteLine($"The current mileage: {_currentMileage}");
+        }
+
+        public void IncrementMileage()
+        {
+            if(_currentMileage < 999999)
+            {
+                _currentMileage++;
+                _mileageCounter++;
+            }
+            else
+            {
+                _currentMileage = 0;
+                _mileageCounter++;
+            }
+        }
+    }
+}
