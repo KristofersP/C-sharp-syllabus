4ba0b0cc2fb44cdf741bde1742f25bf857cc74e4
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-14 17:12:59 +0200

    Exercise4 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Movie
+    {
+        private string _title { get; set; }
+        private string _studio { get; set; }
+        public string Rating { get; private set; }
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = "PG";
+        }
+
+        public override string ToString()
+        {
+            return $"{_title}, {_studio}, {Rating}";
+        }
+
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var spiderMan = new Movie("SpiderMan", "Marvel");
+            var jamesBond = new Movie("James Bond", "Eon Productons", "PG-13");
+            var glass = new Movie("Glass", "Buena Vista International", "PG-13");
+            var movies = new List<Movie>
+            {
+                spiderMan,
+                jamesBond,
+                glass
+            };
+
+            var filtered = GetPg(movies.ToArray());
+
+            foreach (var movie in filtered)
+            {
+                Console.WriteLine($"{movie}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static Movie[] GetPg(Movie[] movies)
+        {
+            var filtered = new List<Movie>();
+
+            foreach (var movie in movies)
+            {
+                if(movie.Rating == "PG")
+                {
+                    filtered.Add(movie);
+                }
+            }
+
+            return filtered.ToArray();
+        }
+    }
+}
