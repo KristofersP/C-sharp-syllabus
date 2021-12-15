b74150f8bf926b07f441f919d51b2990579b8a04
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-15 15:13:59 +0200

    Exercise5 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Date.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+     class Date
+    {
+        private int _month;
+        private int _year;
+        private int _day;
+        public int Month 
+        {
+            get
+            {
+                return _month;
+            }
+            set
+            {
+                if (value > 0 && value < 13)
+                {
+                    _month = value;
+                }
+                else
+                {
+                    _month = 1;
+                }
+            } 
+        }
+        public int Year 
+        { 
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value > -1)
+                {
+                    _year = value;
+                }
+                else
+                {
+                    _year = 1;
+                }
+            } 
+        }
+        public int Day 
+        {
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                if (value > 0 && value < 32)
+                {
+                    _day = value;
+                }
+                else
+                {
+                    _day = 1;
+                }
+            }
+        }
+
+        public Date(int year, int month, int day)
+        {
+            Month = month;
+            Year = year;
+            Day = day;
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{_month}/{_day}/{_year}");
+        }
+    }
+}
