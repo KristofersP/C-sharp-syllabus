e33c837cdbca709282b8cf4d37366f80adb391b1
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-15 16:14:51 +0200

    Exercise7 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/DogTest/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    public class Dog
+    {
+        public string name;
+        public string sex;
+        public string father;
+        public string mother;
+
+        public Dog(string name, string sex, string father, string mother)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        public string FathersName()
+        {
+            return father == null ? "Unknown" : father;
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            return mother == otherDog.mother;
+        }
+    }
+}
