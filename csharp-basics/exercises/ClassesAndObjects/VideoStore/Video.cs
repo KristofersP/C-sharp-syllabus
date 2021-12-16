a7f15405aabb8dd33c09d7b2f6cf0b11b0d7b2c6
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-16 18:13:51 +0200

    Exercise11 doooone.

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,44 +1,59 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace VideoStore
 {
-    class Video
+    public class Video
     {
+        private string _title;
+        private bool _isCheckedOut;
+        private List<double> _rating;
+
         public Video(string title)
         {
-            
+            _title = title;
+            _rating = new List<double>();
         }
 
         public void BeingCheckedOut()
         {
-            
+            _isCheckedOut = true;
         }
 
         public void BeingReturned()
         {
-            
+            _isCheckedOut = false;
         }
 
         public void ReceivingRating(double rating)
         {
-            
+            _rating.Add(rating);
         }
 
         public double AverageRating()
         {
-            return 0;
+            if(_rating != null)
+            {
+                return _rating.Average();
+            }
+            else
+            {
+                return 0;
+            }
+            
         }
 
         public bool Available()
         {
-            return true;
+            return _isCheckedOut ? false : true;
         }
 
-        public string Title => "";
+        public string Title => _title;
 
         public override string ToString()
         {
-            return $"{Title} {AverageRating()} {Available()}";
+            return $"{Title} {Math.Round(AverageRating(),1)} {Available()}";
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -3,36 +3,65 @@ using System.Collections.Generic;
 
 namespace VideoStore
 {
-    class VideoStore
+    public class VideoStore
     {
+        private List<Video> _inventory;
+        private Dictionary<string, Video> movies = new Dictionary<string, Video>();
+
 
         public VideoStore()
         {
-            
+            _inventory = new List<Video>();
         }
 
         public void AddVideo(string title)
         {
-            
+            _inventory.Add(new Video(title));
         }
         
         public void Checkout(string title)
         {
+            foreach (var item in _inventory)
+            {
+                if(item.Title == title)
+                {
+                    item.BeingCheckedOut();
+                    break;
+                }
+            }
 
         }
 
         public void ReturnVideo(string title)
         {
+            foreach (var item in _inventory)
+            {
+                if (item.Title == title)
+                {
+                    item.BeingReturned();
+                    break;
+                }
+            }
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-            
+            foreach (var item in _inventory)
+            {
+                if (item.Title == title)
+                {
+                    item.ReceivingRating(rating);
+                    break;
+                }
+            }
         }
 
         public void ListInventory()
         {
-            
+            foreach(var item in _inventory)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
