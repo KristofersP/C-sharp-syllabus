6519db3b39fb43019735da8463602253228b4b58
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-16 23:27:38 +0200

    Exercise 12 done.

--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,20 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            Account A = new Account("A", 100.00m);
+            Account B = new Account("B", 0.0m);
+            Account C = new Account("V", 0.0m);
+
+            Account.Transfer(A, B, 50.00m);
+            Account.Transfer(B, C, 25.00m);
+
+            Console.WriteLine(A.ToString());
+            Console.WriteLine(B.ToString());
+            Console.WriteLine(C.ToString());
+
+            Console.ReadKey();
         }
+
+       
     }
 }
