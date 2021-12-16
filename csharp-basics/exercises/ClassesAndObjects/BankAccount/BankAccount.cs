80e41ca9e22ba4e85cd0a4ec646258793e923bf8
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-16 13:48:11 +0200

    Exercise 10 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private string _name;
+        private decimal _balance;
+
+        public BankAccount(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            if(_balance < 0)
+            {
+                _balance *= -1;
+                return $"{_name}, -${_balance.ToString("0.00")}";
+            }
+            else
+            {
+                return $"{_name}, ${_balance.ToString("0.00")}";
+            }
+        }
+    }
+}
