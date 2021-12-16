f7df5f9e5690a6fd669fb82059a9f013b1d257d2
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-16 13:34:49 +0200

    Exercise9 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        public static void Main()
+        {
+            SwapPoints p1 = new SwapPoints(1, 3);
+            SwapPoints p2 = new SwapPoints(2, 4);
+
+            p1.swapPoints(p1, p2);
+
+            Console.WriteLine($"({p1.x}, {p1.y})");
+            Console.WriteLine($"({p2.x}, {p2.y})");
+        }
+        
+    }
+}
