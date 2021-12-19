4a82950957df5f7f0769d8a70f8ae79eef6f16b0
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-19 17:05:44 +0200

    Exercise1 done.

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -8,25 +8,31 @@ namespace Exercise1
 {
     class Program
     {
-        /**
-           * Origination:
-           * Audi -> Germany
-           * BMW -> Germany
-           * Honda -> Japan
-           * Mercedes -> Germany
-           * VolksWagen -> Germany
-           * Tesla -> USA
-           */
-
         private static void Main(string[] args)
         {
             string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
 
-            //todo - replace array with an List and print out the results
+            List<string> list = new List<string>(array);
+            list.ForEach(Console.WriteLine);
 
-            //todo - replace array with a HashSet and print out the results
+            Console.WriteLine();
 
-            //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+            HashSet<string> set = new HashSet<string>(array);
+            Console.WriteLine(string.Join(" ", set));
+
+            Console.WriteLine();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("Audi", "Germany");
+            dict.Add("BMW", "Germany");
+            dict.Add("Honda", "Japan");
+            dict.Add("Mercedes", "Germany");
+            dict.Add("VolksWagen", "Germany");
+            dict.Add("Tesla", "USA");
+
+            dict.ToList().ForEach(x => Console.WriteLine(x.Key));
+
+            Console.ReadKey();
         }
     }
 }
