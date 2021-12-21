72cfff50c954feda9898562ce2adece16c6aafd7
Kristofers Petersons <kristofersp2@gmail.com>
2021-12-21 21:57:47 +0200

    Exercise9 done.

---
+++ csharp-basics/exercises/Collections/Exercise9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("a");
+            set.Add("b");
+            set.Add("c");
+            set.Add("d");
+            set.Add("e");
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in set)
+            {
+                set.Remove(item);
+            }
+
+            Console.WriteLine();
+
+            set.Add("a");
+            set.Add("a");
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
+
