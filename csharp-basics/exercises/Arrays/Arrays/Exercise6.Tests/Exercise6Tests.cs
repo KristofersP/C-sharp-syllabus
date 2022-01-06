c9c635c1238e149154c4c470eceb55d95cc1213f
Kristofers Petersons <kristofersp2@gmail.com>
2022-01-06 17:48:56 +0200

    Fixed the first Test exercise

--- csharp-basics/exercises/Arrays/Arrays/Exercise6.Tests/Exercise6Tests.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6.Tests/Exercise6Tests.cs
@@ -15,22 +15,31 @@ namespace Exercise6.Tests
         [Test]
         public void MakeArray1_LengthShouldBe10()
         {
+            //Act
             var result = _target.MakeArray1();
+
+            //Assert
             Assert.AreEqual(10, result.Length);
         }
 
         [Test]
         public void MakeArray2_LengthShouldBe10()
         {
+            //Act
             var result = _target.MakeArray2();
+
+            //Assert
             Assert.AreEqual(10, result.Length);
         }
 
         [Test]
         public void ChangeArray_Given7_LastElementShouldBe7()
         {
+            //Act
             var arr = _target.MakeArray1();
             var result = _target.ChangeArray(arr);
+
+            //Assert
             Assert.AreEqual(-7, result[9]);
         }
     }
