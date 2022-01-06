9049b168d8080654570e213c4fca88bf870f1671
Kristofers Petersons <kristofersp2@gmail.com>
2022-01-06 17:46:56 +0200

    Test second exercise done.

--- csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -2,24 +2,28 @@
 
 namespace Exercise1
 {
-    class Program
+    public class Program
     {
+        private static int _num1;
+        private static int _num2;
         static void Main()
         {
             Console.WriteLine("Enter the first number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            _num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            _num2 = Convert.ToInt32(Console.ReadLine());
+        }
 
-            if (num1 == 15 || num2 == 15 || num1 + num2 == 15 || num1 - num2 == 15 || num2 - num1 == 15) 
+        public bool CheckNumber15(int num1, int num2)
+        {
+            if (num1 == 15 || num2 == 15 || num1 + num2 == 15 || num1 - num2 == 15 || num2 - num1 == 15)
             {
-                Console.WriteLine("True");
-            } 
-             else 
-            {
-                Console.WriteLine("False");
+                return true;
             }
-            
+            else
+            {
+                return false;
+            }
         }
     }
 }
