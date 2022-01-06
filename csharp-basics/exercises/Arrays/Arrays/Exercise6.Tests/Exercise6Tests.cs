bfe1bd0d12a4f549043d63ea649f6c1e2c9d1fba
Kristofers Petersons <kristofersp2@gmail.com>
2022-01-06 17:26:00 +0200

    First Test exercise done.

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6.Tests/Exercise6Tests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Exercise6;
+
+namespace Exercise6.Tests
+{
+    public class Tests
+    {
+        private Exercise6Array _target;
+        [SetUp]
+        public void Setup()
+        {
+             _target = new Exercise6Array();
+        }
+
+        [Test]
+        public void MakeArray1_LengthShouldBe10()
+        {
+            var result = _target.MakeArray1();
+            Assert.AreEqual(10, result.Length);
+        }
+
+        [Test]
+        public void MakeArray2_LengthShouldBe10()
+        {
+            var result = _target.MakeArray2();
+            Assert.AreEqual(10, result.Length);
+        }
+
+        [Test]
+        public void ChangeArray_Given7_LastElementShouldBe7()
+        {
+            var arr = _target.MakeArray1();
+            var result = _target.ChangeArray(arr);
+            Assert.AreEqual(-7, result[9]);
+        }
+    }
+}
--- csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -2,29 +2,54 @@
 
 namespace Exercise6
 {
-    class Program
+    public class Exercise6Array
     {
-        private static void Main(string[] args)
-        {
-            int[] myArray1 = new int[10];
-            int[] myArray2 = new int[10];
+        public static int[] _myArray1 = new int[10];
+        public static int[] _myArray2 = new int[10];
 
+        public Exercise6Array()
+        {
+            
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public int[] MakeArray1()
+        {
             Random rnd = new Random();
 
             for (int i = 0; i < 10; i++)
             {
-                myArray1[i] = rnd.Next(1, 101);
+                _myArray1[i] = rnd.Next(1, 101);
             }
 
+            return _myArray1;
+        }
+
+        public int[] MakeArray2()
+        {
             for (int i = 0; i < 10; i++)
             {
-                myArray2[i] = myArray1[1];
+                _myArray2[i] = _myArray1[i];
             }
 
-            myArray1[9] = -7;
-
-            Console.WriteLine("Array 1: " + string.Join(",", myArray1));
-            Console.WriteLine("Array 2: " + string.Join(",", myArray2));
+            return _myArray2;
         }
+
+        public int[] ChangeArray(int[] array)
+        {
+            array[9] = -7;
+
+            return array;
+        }
+
+        public string PrintArray(int[] myArray)
+        {
+            return "Array: " + string.Join(",", myArray);
+        }
+        
+
     }
 }
