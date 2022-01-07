97f6785ec46ffc5542bedd7a1f67a4ada5bb3052
Kristofers Petersons <kristofersp2@gmail.com>
2022-01-07 22:53:14 +0200

    Test exercise5 done.

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -3,18 +3,21 @@ using System;
 
 namespace GravityCalculator
 {
-    class Program
+    public class Program
     {
+        private static double _gravity = 9.81;
+        private static double _fallingTime = 10.0;
+
         static void Main(string[] args)
         {
-            double gravity = -9.81;
-            double initialVelocity = 0.0;
-            double fallingTime = 10.0;
-            double initialPosition = 0.0;
-            double finalPosition = (gravity * fallingTime * fallingTime)/2 ;
-
-            Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
+            var calc = new Program();
+            Console.WriteLine("The object's position after " + _fallingTime + " seconds is " + calc.CalculateFinalPosition(_gravity, _fallingTime) + " m.");
             Console.ReadKey();
+        }
+
+        public double CalculateFinalPosition(double gravity, double fallingTime)
+        {
+            return (gravity * fallingTime * fallingTime) / 2;
         }
     }
 }
